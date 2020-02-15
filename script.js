c9640b45018b6130f2996a7a317a3073aa42792c
Atul Mahajan <luta_atul@hotmail.com>
2020-02-16 01:55:56 +1100

    Created function generatePassword and called function writePassword.

--- script.js
+++ script.js
@@ -1,24 +1,29 @@
 // Assignment Code
+
 var generateBtn = document.querySelector("#generate");
 
-var passwordLength = prompt(
-  "Please choose length of your password.\n Enter a number between 8 - 128"
-);
-var passwordLowerCase = prompt(
-  "What is your password character type: \n Do you want to include lowercase characters: \n Type Y for YES or  N for NO"
-);
+writePassword();
 
-var passwordUpperCase = prompt(
-  "Do you want to include uppercase characters: \n Type Y for YES or  N for NO"
-);
+function generatePassword() {
+  var passwordLength = prompt(
+    "Please choose length of your password.\n Enter a number between 8 - 128"
+  );
+  var passwordLowerCase = prompt(
+    "What is your password character type: \n Do you want to include lowercase characters: \n Type Y for YES or  N for NO"
+  );
 
-var passwordNumeric = prompt(
-  "Do you want to include numbers: \n Type Y for YES or  N for NO"
-);
+  var passwordUpperCase = prompt(
+    "Do you want to include uppercase characters: \n Type Y for YES or  N for NO"
+  );
 
-var passwordSpecialCharacters = prompt(
-  "Do you want to include special characters: \n Type Y for YES or  N for NO"
-);
+  var passwordNumeric = prompt(
+    "Do you want to include numbers: \n Type Y for YES or  N for NO"
+  );
+
+  var passwordSpecialCharacters = prompt(
+    "Do you want to include special characters: \n Type Y for YES or  N for NO"
+  );
+}
 
 // Write password to the #password input
 function writePassword() {
