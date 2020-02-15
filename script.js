68d4a571bd97e45adda9e6ab3f8b075475d2846c
Atul Mahajan <luta_atul@hotmail.com>
2020-02-16 03:31:59 +1100

    Generating random lower case characters.

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 // Assignment Code
 
+// var array = new Uint32Array(10);
+// // var mixPass = {
+// //   number: 12,
+// //   lowerCaseCharacter: "adddd",
+// //   upperCaseCharacter: "UUULKL",
+// //   specialCharacters: "%$#&&"
+// // };
+// window.crypto.getRandomValues(array);
+
+// alert("Your lucky numbers:");
+// for (var i = 0; i < array.length; i++) {
+//   alert(array[i]);
+//   //  alert("Object is: " + mixPass.number);
+// }
+
 var generateBtn = document.querySelector("#generate");
 var passwordLength;
 var passwordLowerCase;
@@ -27,11 +42,45 @@ function prompts() {
     "Please choose length of your password.\n Enter a number between 8 - 128"
   );
 
-  var randomNumberLength = Math.floor(
-    Math.random() * Math.floor(passwordLength)
-  );
-  alert(randomNumberLength);
+  // var randomNumberLength = Math.floor(
+  //   Math.random() * Math.floor(passwordLength)
+  // );
 
+  // alert(randomNumberLength);
+
+  // randomNumberLength = Math.random()
+  //   .toString(36)
+  //   .substring(7);
+  // alert("Using substring: " + randomNumberLength);
+  // // var array = new Uint32Array(randomNumberLength);
+
+  // var randomValue = window.crypto.getRandomValues(
+  //   new Uint32Array(passwordLength)
+  // );
+
+  // alert("Your lucky numbers:" + randomValue);
+
+  alert(
+    "Generating random Lower characters:" +
+      Math.random()
+        .toString(36)
+        .replace(/[^a-z]+/g, "")
+        .substr(0, passwordLength)
+  );
+  // for (var i = 0; i < array.length; i++) {
+  //   alert(array[i]);
+  // }
+
+  // alert(
+  //   "New way:" +
+  //     Math.random()
+  //       .toString(36)
+  //       .substr(2, 5) +
+  //     "\n" +
+  //     Math.random()
+  //       .toString(36)
+  //       .substr(2, 5)
+  // );
   passwordLowerCase = prompt(
     "What is your password character type: \n Do you want to include lowercase characters: \n Type Y for YES or  N for NO"
   );
