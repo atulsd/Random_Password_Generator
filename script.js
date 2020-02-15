a9b79dae407c2430298842f39310ce160a377acd
Atul Mahajan <luta_atul@hotmail.com>
2020-02-16 04:25:35 +1100

    Added conditions to check user input.

--- script.js
+++ script.js
@@ -14,6 +14,23 @@
 //   alert(array[i]);
 //   //  alert("Object is: " + mixPass.number);
 // }
+function randomString(length, chars) {
+  alert("length is" + length + "Required" + chars);
+  var mask = "";
+  if (chars.indexOf("a") > -1) mask += "abcdefghijklmnopqrstuvwxyz";
+  if (chars.indexOf("A") > -1) mask += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  if (chars.indexOf("#") > -1) mask += "0123456789";
+  if (chars.indexOf("!") > -1) mask += "~`!@#$%^&*()_+-={}[]:\";'<>?,./|\\";
+  var result = "";
+  for (var i = length; i > 0; --i)
+    result += mask[Math.floor(Math.random() * mask.length)];
+  alert("we are Almost there mate");
+  return result;
+}
+
+// alert(randomString(16, "aA"));
+// alert(randomString(32, "#aA"));
+//alert(randomString(10, "a#A!"));
 
 var generateBtn = document.querySelector("#generate");
 var passwordLength;
@@ -21,22 +38,30 @@ var passwordLowerCase;
 var passwordUpperCase;
 var passwordNumeric;
 var passwordSpecialCharacters;
+var required = "";
 
 writePassword();
 
 function generatePassword() {
-  prompts();
-  //return window.crypto.getRandomValues();
-  return (
-    passwordLength +
-    passwordLowerCase +
-    passwordUpperCase +
-    passwordNumeric +
-    passwordSpecialCharacters
+  prompts(passwordLength, required);
+  alert(
+    "After prompts length is:" + passwordLength + " Required is: " + required
   );
+  //return window.crypto.getRandomValues();
+  var result = randomString(passwordLength, required);
+  alert("Value inside generatePassword is:" + result);
+  return result;
+
+  // return (
+  //   passwordLength +
+  //   passwordLowerCase +
+  //   passwordUpperCase +
+  //   passwordNumeric +
+  //   passwordSpecialCharacters
+  // );
 }
 
-function prompts() {
+function prompts(passwordLength, required) {
   alert("Prompt function.");
   passwordLength = prompt(
     "Please choose length of your password.\n Enter a number between 8 - 128"
@@ -60,21 +85,21 @@ function prompts() {
 
   // alert("Your lucky numbers:" + randomValue);
 
-  alert(
-    "Generating random Lower characters: " +
-      Math.random()
-        .toString(36)
-        .replace(/[^a-z]+/g, "")
-        .substr(0, passwordLength)
-  );
-  alert(
-    "Generating random Upper characters: " +
-      Math.random()
-        .toString(36)
-        .replace(/[^a-z]+/g, "")
-        .substr(0, passwordLength)
-        .toUpperCase()
-  );
+  // alert(
+  //   "Generating random Lower characters: " +
+  //     Math.random()
+  //       .toString(36)
+  //       .replace(/[^a-z]+/g, "")
+  //       .substr(0, passwordLength)
+  // );
+  // alert(
+  //   "Generating random Upper characters: " +
+  //     Math.random()
+  //       .toString(36)
+  //       .replace(/[^a-z]+/g, "")
+  //       .substr(0, passwordLength)
+  //       .toUpperCase()
+  // );
 
   // for (var i = 0; i < array.length; i++) {
   //   alert(array[i]);
@@ -94,17 +119,31 @@ function prompts() {
     "What is your password character type: \n Do you want to include lowercase characters: \n Type Y for YES or  N for NO"
   );
 
+  if (passwordLowerCase === "y" || "Y") {
+    required = "a";
+  }
   passwordUpperCase = prompt(
     "Do you want to include uppercase characters: \n Type Y for YES or  N for NO"
   );
-
+  if (passwordUpperCase === "y" || "Y") {
+    required += "A";
+  }
   passwordNumeric = prompt(
     "Do you want to include numbers: \n Type Y for YES or  N for NO"
   );
-
+  if (passwordNumeric === "y" || "Y") {
+    required += "#";
+  }
   passwordSpecialCharacters = prompt(
     "Do you want to include special characters: \n Type Y for YES or  N for NO"
   );
+  if (passwordSpecialCharacters === "y" || "Y") {
+    required += "!";
+  }
+  alert("Password length is " + passwordLength + " Required is: " + required);
+  return passwordLength, required;
+  //return generatePassword(passwordLength, required);
+  //randomString(passwordLength, required);
 }
 
 // Write password to the #password input
